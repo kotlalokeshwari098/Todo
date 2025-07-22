fd3d9a9fcede5aaba790c41f297cdbc4d5aaadff
Kotla.Lokeshwari <kotlalokeshwari1871@gmail.com>
2025-07-23 00:11:45 +0530

    Added the input fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useState } from "react"
+import InputField from "./components/InputField"
+import type { Todo } from "./model";
 
-const App = () => {
+const App : React.FC = () => {
+  const [todo,setTodo]=useState<string>('');
+  const [todos,setTodos]=useState<Todo[]>([]) 
+
+  const handleAdd=(e:React.FormEvent)=>{
+       e.preventDefault();
+       
+       
+         setTodos([...todos,{id:Date.now(),todo:todo,isDone:false}])
+         setTodo(' ')
+       console.log(todos)
+       
+  }
   return (
-    <div>App</div>
+    <div className="App">
+       <span className="heading">Taskify</span>
+       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+    </div>
   )
 }
 
