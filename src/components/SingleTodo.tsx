12814ca2f4fb300e71a46e57078e38a6b5a6c114
Kotla.Lokeshwari <kotlalokeshwari1871@gmail.com>
2025-07-26 14:31:11 +0530

    Added edit feature

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,43 +1,74 @@
-import React from 'react'
-import type { Todo } from '../model'
-import {AiFillEdit,AiFillDelete} from 'react-icons/ai';
-import {MdDone} from 'react-icons/md';
+import React, { useEffect, useRef, useState } from "react";
+import type { Todo } from "../model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
 
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
 
-type Props={
-   todo:Todo,
-   todos:Todo[],
-   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
-}
+const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
 
-const SingleTodo:React.FC<Props> = ({todo,todos,setTodos}) => {
-  const handleDone=(id:number)=>{
-     setTodos(
-      todos.map((todo)=>
-      todo.id===id ? {...todo,isDone:!todo.isDone} : todo))
-  }
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
 
-  const handleDelete=(id:number)=>{
-      setTodos(todos.filter((todo)=>todo.id!==id));
-  }
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e:React.FormEvent,id:number) => {
+     e.preventDefault();
+
+     setTodos(todos.map((todo)=>(
+      todo.id===id?{...todo,todo:editTodo}:todo
+     )))
+     setEdit(false)
+  };
+  const inputRef=useRef<HTMLInputElement>(null);
+
+  useEffect(()=>{
+      inputRef.current?.focus();
+  },[edit])
+
   return (
-    <form action="" className='flex gap-5'>
-      {todo.isDone?(
-         <span><s>{todo.todo}</s></span>
-      ):(
+    <form action="" className="flex gap-5" onSubmit={(e)=>handleEdit(e,todo.id)}>
+      {edit ? (
+        <input value={editTodo} ref={inputRef} onChange={(e)=>setEditTodo(e.target.value)} className=""/>
+      ) : todo.isDone ? (
+        <span>
+          <s>{todo.todo}</s>
+        </span>
+      ) : (
         <span>{todo.todo}</span>
       )}
-      
-       <div className='flex gap-5'>
-        <span><AiFillEdit /></span>
-        <span onClick={()=>handleDelete(todo.id)}><AiFillDelete /></span>
-        <span onClick={()=>handleDone(todo.id)}>
-          <MdDone />
-          
+
+      <div className="flex gap-5">
+        <span onClick={() => 
+        {
+          if(!edit && !todo.isDone) {
+          setEdit(!edit)
+        };
+    }}>
+          <AiFillEdit />
         </span>
-       </div>
+        <span onClick={() => handleDelete(todo.id)}>
+          <AiFillDelete />
+        </span>
+        <span onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+      </div>
     </form>
-  )
-}
+  );
+};
 
 export default SingleTodo;
